2788f1a740ae0e6d5927462576df09bea999ec68
cleovacalista <cleovacalistaazizakayviar@students.amikom.ac.id>
2025-05-24 23:54:15 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
   <section class="bg-black text-white">
     <HeroSection />
     <IntroSection />
-    <LayananSection />
     <CtaSection />
   </section>
 </template>
@@ -11,5 +10,4 @@
 import HeroSection from '@/components/HeroSlider.vue'
 import IntroSection from '@/components/HomeIntroSection.vue'
 import CtaSection from '@/components/HomeCtaSection.vue'
-import LayananSection from '@/components/LayananComponent.vue'
 </script>
